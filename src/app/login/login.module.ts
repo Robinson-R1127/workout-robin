013f4661aadd7e77fe04bf3ce4f5ea0ba9bc6375
Robinson-R1127 <rabe.robinson01@gmail.com>
2020-04-08 13:11:57 +0900

    feat(login): ログイン画面追加 #17

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule, FormsModule]
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatToolbarModule
+  ]
 })
 export class LoginModule {}
