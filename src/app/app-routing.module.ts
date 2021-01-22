11371cb8f3189a2b89e066e0a206076b95b472ac
Robinson-R1127 <rabe.robinson01@gmail.com>
2021-01-22 18:52:58 +0900

    fix(auth): revert作業 #106

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
