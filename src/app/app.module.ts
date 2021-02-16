ff76b3afc4fea36490792711170e4d707bd3710a
Robinson-R1127 <rabe.robinson01@gmail.com>
2021-02-16 15:40:24 +0900

    feat(header): header実装 #136

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,15 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { PostDialogComponent } from './post-dialog/post-dialog.component';
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, PostDialogComponent, NotFoundComponent],
+  declarations: [
+    AppComponent,
+    PostDialogComponent,
+    NotFoundComponent,
+    HeaderComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
